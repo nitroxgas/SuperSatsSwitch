2176df6c2460ca0851f96827549e327f5cd5570c
George Alexandre Silva <nitroxgas@gmail.com>
2023-10-08 22:36:30 -0300

    More polish

--- include/lnpay.h
+++
@@ -1,7 +0,0 @@
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include <ArduinoJson.h>
-
-String consultaWallet(WiFiClient WiFic, String walletkey, String accessKey);
-DynamicJsonDocument buscaGotas(String walletAddress, int startBlock, int endBlock);
-DynamicJsonDocument buscaImgGotas(const char* ipfsAddr);
